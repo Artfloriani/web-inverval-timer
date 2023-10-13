70f18637b20953dfc8787cc061d085684d7ed247
Arthur Floriani <artfloriani@gmail.com>
2023-10-13 11:11:27 -0500

    feat: add initial logic and layout for timer

---
+++ src/lib/Button.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    export let icon :string;
+    export let label: string;
+    export let text : string = '';
+
+
+ import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+</script>
+
+<ion-button class="button" on:click={() => {
+     dispatch('click')
+}}>
+    <ion-icon src={icon} class="icon"></ion-icon>
+    <span class="text">{label}</span>
+    <span class="additional-text">{text}</span>
+</ion-button>
+
+<style>
+    .button {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.5rem;
+        border: none;
+        color: #fff;
+        font-size: 1rem;
+        cursor: pointer;
+    }
+
+    .icon {
+        margin-right: 0.5rem;
+    }
+
+    .text {
+        margin-right: auto;
+    }
+
+    .additional-text {
+        margin-left: 0.5rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,91 @@
+<script lang="ts">
+	import Button from "$lib/Button.svelte";
 
-  <ion-card>
-	<ion-card-header>
-		<ion-card-subtitle>Great success!!</ion-card-subtitle>
-		<ion-card-title>Welcome to your app!</ion-card-title>
-	</ion-card-header>
+	import {playOutline, timerOutline, pauseCircleOutline, refreshCircleOutline} from "ionicons/icons";
 
-	<ion-card-content>
-		Thank you for using this starter. Click buttons below to open these guides (will
-		open in new window). Don't forget to open DevTools to see this app in mobile mode. Happy coding!!!
-	</ion-card-content>
 
-	<ion-item>
-		<ion-label>Visit Ionic Showcase app with sourceviewer</ion-label>
-		<ion-button href="https://ionicsvelte.firebaseapp.com/" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
 
-	<ion-item>
-		<ion-label>Visit Ionic component docs</ion-label>
-		<ion-button
-			href="https://ionicframework.com/docs/components"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte Kit docs</ion-label>
-		<ion-button
-			href="https://kit.svelte.dev/docs/introduction"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte docs</ion-label>
-		<ion-button href="https://svelte.dev/docs" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
-</ion-card>
+	// timer settings
+	let workTime = 180;
+	let restTime = 60;
+	let rounds = 3;
+	let timer = (workTime + restTime) * rounds;
+	
+	// labels for timer settings
+	let workLabel = "3:00";
+	let restLabel = "1:00"
+	let timerLabel = '4:00';
 
-  
+	// running timer values
+	let currentRound = 0;
+	let step : 'settings' | 'pre' | 'work' | 'rest' | 'finished' = 'settings';
+
+	$: {
+		//convert seconds to "mm:ss" format
+		workLabel = secondsToHHmm(workTime);
+		restLabel = secondsToHHmm(restTime);
+		timerLabel = secondsToHHmm(timer)
+	}
+
+	function startTimer() {
+		setInterval(() => {
+			timer--;
+		}, 1000);
+	}
+
+	function secondsToHHmm(seconds: number)  {
+		let m = Math.floor(seconds / 60);
+		let s = seconds - m * 60;
+		return s < 10 ? `${m}:0${s}`:`${m}:${s}`;
+	}
+
+</script>
+
+<ion-content >
+	<div class="layout">
+
+		<div class="timer__header">
+			<div class="timer__number">{timerLabel}</div>
+			
+		</div>
+		<div class="timer__content"> 
+			<Button icon={timerOutline} label="Work" text={workLabel} />
+			<Button icon={pauseCircleOutline} label="Rest" text={restLabel} />
+			<Button icon={refreshCircleOutline} label="Rounds" text="3" />
+			<Button icon={playOutline} label="Start" on:click={startTimer} />
+		</div>
+	</div>
+</ion-content>
+
+<style>
+
+	.layout {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+	.timer__header {
+		flex: 1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #3880ff;
+		color: #fff;
+	}
+
+	.timer__content {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
+	.timer__number {
+		font-size: 40px;
+		font-weight: bold;
+	}
+
+	.number {
+		float: right;
+	}
+</style>
